01be41f5c14bab6067454743587e096a6024958d
Vlad Kyianytsia <kiyanitsa2002021@gmail.com>
2024-03-05 15:39:48 +0200

    created DishType & Dish models

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,21 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class DishType(models.Model):
+    name = models.CharField(max_length=65)
+
+    def __str__(self):
+        return self.name
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=65)
+    description = models.TextField()
+    price = models.DecimalField(decimal_places=2, max_digits=5)
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
+    cooks = models.ManyToManyField(settings.AUTH_USER_MODEL)
+
+    def __str__(self):
+        return self.name
