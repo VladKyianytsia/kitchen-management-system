5c837966da9b85b1f0a096d55034c1eb5d4c67ad
Vlad Kyianytsia <kiyanitsa2002021@gmail.com>
2024-03-06 11:10:54 +0200

    added registration page + registration form & view

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
+from kitchen.views import register
 
-app_name = 'kitchen'
+app_name = "kitchen"
 
 urlpatterns = [
-
+    path("register/", register, name="register")
 ]
--- kitchen/views.py
+++ kitchen/views.py
@@ -1,3 +1,33 @@
+from django.contrib.auth import get_user_model, login
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from kitchen.forms import RegistrationForm
+
+
+def register(request: HttpRequest) -> HttpResponse:
+    if request.method == 'GET':
+        form = RegistrationForm()
+        return render(
+            request,
+            "kitchen/cook_registration.html",
+            context={
+                "form": form
+            }
+        )
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.save()
+            login(request, user)
+            return HttpResponse("<h1>Welcome to Kitchen</h1>")
+        else:
+            return render(
+                request,
+                "kitchen/cook_registration.html",
+                context={
+                    "form": form
+                }
+            )
