708badd7584e206d02c786fd4894eb1ff28ec3a7
Vlad Kyianytsia <kiyanitsa2002021@gmail.com>
2024-03-06 08:09:16 +0200

    Updated Dish model(added Meta & related names)

--- kitchen/models.py
+++ kitchen/models.py
@@ -14,8 +14,18 @@ class Dish(models.Model):
     name = models.CharField(max_length=65)
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=5)
-    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
-    cooks = models.ManyToManyField(settings.AUTH_USER_MODEL)
+    dish_type = models.ForeignKey(
+        DishType,
+        on_delete=models.CASCADE,
+        related_name="dishes"
+    )
+    cooks = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="dishes"
+    )
+
+    class Meta:
+        verbose_name_plural = "Dishes"
 
     def __str__(self):
         return self.name
